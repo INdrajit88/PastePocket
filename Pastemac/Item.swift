34053864c70b1d3b4942b55885380c1e5fb9d67f
Indrajit Ari <indrajit>
2026-03-20 19:37:06 +0530

    Initial Commit

---
+++ Pastemac/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Pastemac
+//
+//  Created by Indrajit Ari on 20/03/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
